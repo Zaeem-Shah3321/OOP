18de130e58038ad742b89740dcb9371fa947b25f
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2024-01-23 15:39:04 +0500

    Classes

---
+++ OOP Labs/OOP Lab2/Task_1_a_Class.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class student
+    {
+        public student(string name_r , int fsc_r , int m_r)
+        {
+            name = name_r;
+            fscMarks = fsc_r;
+            mMarks = m_r;
+
+        }   
+        public string name;
+        public int fscMarks;
+        public int mMarks;
+    }
+}
