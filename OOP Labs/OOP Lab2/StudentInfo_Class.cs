1e3df32ca9afe5c90dae750a5771666f64c536e2
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2024-01-23 14:20:41 +0500

    Introduction To Classes

---
+++ OOP Labs/OOP Lab2/StudentInfo_Class.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    public class student
+    {
+        public string name;
+        public int fscMarks;
+        public int metricMarks;
+    }
+}
