96aa01a7c33bf22a7708dd57a17f9c3291c577e7
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2024-02-04 20:01:46 +0500

    Challange 2

---
+++ OOP Labs/OOP Lab 3/Products_Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2
+{
+    class Product
+    {
+        public int ID;
+        public string Name;
+        public float Price;
+        public string Category;
+        public string BrandName;
+        public string Country;
+        public Product(int iD, string name, float price, string category, string brandName, string country)
+        {
+            ID = iD;
+            Name = name;
+            Price = price;
+            Category = category;
+            BrandName = brandName;
+            Country = country;
+        }
+    }
+}
