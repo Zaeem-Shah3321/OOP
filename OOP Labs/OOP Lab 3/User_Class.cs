65042dac42000ed2a14457d01b093322ef09ff46
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2024-02-04 21:49:12 +0500

    Challange 3

---
+++ OOP Labs/OOP Lab 3/User_Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challanege3
+{
+    public class users
+    {
+        public string Username;
+        public string Password;
+        public string Role;
+
+        public users(string username, string password, string role)
+        {
+            Username = username;
+            Password = password;
+            Role = role;
+        }
+    }
+
+}
