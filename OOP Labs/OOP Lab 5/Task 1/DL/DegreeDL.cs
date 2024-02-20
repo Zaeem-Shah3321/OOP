06318addec527cb4ccd2e25febd2a3b1bb29b483
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2024-02-20 19:14:25 +0500

    task

---
+++ OOP Labs/OOP Lab 5/Task 1/DL/DegreeDL.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using labtask1.BL;
+using labtask1.UI;
+
+
+namespace labtask1.DL
+{
+    internal class DegreeDL
+    {
+    }
+}
